27c879148e8a08dddd2188bdaee1f18f43fff1b2
DUSHIME Dan Paul <danpauldushime03@gmail.com>
2024-09-10 11:23:50 +0200

    wrote functions for the unittets

--- tests/test_console.py
+++ tests/test_console.py
@@ -33,7 +33,7 @@ class TestHBNBCommand_prompting(unittest.TestCase):
 
 
 class TestHBNBCommand_help(unittest.TestCase):
-    """Unittests for testing help messages of the HBNB command interpreter."""
+    """Unittests for testing the HBNB command interpreter."""
 
     def test_help_quit(self):
         h = "Quit command to exit the program."
